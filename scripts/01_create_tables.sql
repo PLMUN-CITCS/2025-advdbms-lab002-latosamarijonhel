6d339291acca2a3d905868560e1a960f4b1543f6
Marijonhel latosa <latosamarijonhel_bsit@plmun.edu.ph>
2025-02-26 17:44:57 +0800

    Completed assignment: Created tables and inserted data

--- scripts/01_create_tables.sql
+++ scripts/01_create_tables.sql
@@ -1,3 +1,4 @@
+-- Creating tblStudents table
 /*
     Filename:       01_create_tables.sql
     Purpose:        Create tables for the database
@@ -10,3 +11,10 @@
 
     2. Place your create table query below this comment
 */
+-- Creating tblStudents table
+CREATE TABLE tblStudents (
+    student_id INT PRIMARY KEY AUTO_INCREMENT,
+    student_name VARCHAR(50),
+    student_email VARCHAR(50),
+    student_phone VARCHAR(50)
+);
